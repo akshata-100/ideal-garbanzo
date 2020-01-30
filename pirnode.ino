6c0efa1e1ff953a3dd31a31267371674cd2b64fb
akshata-100 <60460759+akshata-100@users.noreply.github.com>
2020-01-30 12:33:26 +0530

    Add files via upload

---
+++ pirnode.ino
@@ -0,0 +1,19 @@
+#define ledPin D7 // Red LED
+#define pirPin D1 
+
+int pirValue; // variable to store read PIR Value
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+  digitalWrite(ledPin, LOW);
+}
+
+void loop() 
+{
+  pirValue = digitalRead(pirPin);
+  digitalWrite(ledPin, pirValue);
+  Serial.print("pirValue");
+  Serial.println(pirValue);
+}
